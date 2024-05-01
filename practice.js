90f0d83c14f64f74c3ed1a4b8f4d2de08fd0ea17
tonmoybosunia24 <mdtonmoybosunia24@gmail.com>
2024-05-01 16:48:13 +0600

    Full Module Complete With practice

--- practice.js
+++ practice.js
@@ -89,3 +89,96 @@
 //               console.log("You Are My Black Beauty")
 //               break;
 // }
+
+// function startFan(){
+//        console.log("Stand Up")
+//        console.log("Walk Towards The Switch")
+//        console.log("Press The Switch")
+// }
+// startFan();
+
+// function bringSingra(money){
+//        console.log("Mama Singra Den")
+//        console.log(money)
+// }
+// bringSingra(200);
+// function friendsName(name){
+//        console.log(name, "Tor Dawat Thaklo")
+// }
+// friendsName("rohim");
+// friendsName("sohan");
+// function numbers(num1, num2, num3, num4, num5, num6, num7){
+//        var total = num1 + num2 + num3 + num4 + num5 + num6 + num7;
+//        console.log(total);
+// }
+// numbers(2, 5, 4, 7, 8, 3, 7);
+// function add(num1, num2){
+//        console.log(num1, num2);
+//        var sum = num1 + num2
+//        return sum;
+// }
+// var total = add(5, 7);
+// console.log(total);
+// function quantity(money){
+//        var singraPrice = 10;
+//        var quantity = money / singraPrice;
+//        return quantity;
+// }
+// var singraPabo = quantity(200);
+// console.log(singraPabo)
+// function getTotal(assingment1, assingment2, assingment3){
+//        console.log(assingment1, assingment2, assingment3);
+//        var total = assingment1 + assingment2 + assingment3;
+//        var avarage = total / 3;
+//        return avarage;
+// }
+// var avaragePabo = getTotal(70, 80, 90);
+// console.log(avaragePabo);
+// var student = { id: 088, name: "Tonmoy Bosunia", class: 12, Mark: 90};
+// var mobile = { brand: "Samsung", price: 1900, storage: "64GB", camera: "7MP"};
+// var computer = { brand: "walton", Price: 15000, color: "black", processor: "10ThGen"}
+// computer.brand = "Asus"
+// console.log(computer.color)
+// console.log(student);
+// console.log(mobile)
+// console.log(computer)
+// var shoppingCard = {books: 10, sunglass: 5, pen: 4, keyboard: 1, khata: 6, mouse: 2};
+// console.log(shoppingCard.khata);
+// var sunglassQuantity = shoppingCard["sunglass"];
+// console.log(sunglassQuantity)
+// var propertyName = "mouse";
+// var propertyValue = shoppingCard[propertyName];
+// console.log(propertyName, propertyValue)
+// var properties = Object.keys(shoppingCard)
+// console.log(properties)
+// var propertyValue = Object.values(shoppingCard);
+// console.log(propertyValue)
+// shoppingCard.khata = 10;
+// console.log(shoppingCard)
+// shoppingCard["mouse"] = 5;
+// console.log(shoppingCard);
+// var propertyName = "pen"
+// shoppingCard.propertyName = 8;
+// console.log(shoppingCard);
+// var shoppingCard = {book: 10, sunglass: 5, pen: 4, keyboard: 1, khata: 6, mouse: 2};
+// var keys = Object.keys(shoppingCard)
+// console.log(keys);
+// var values = Object.values(shoppingCard);
+// console.log(values);
+// for(var i = 0; i < keys.length; i++){
+//        var propertyName = keys[i];
+//        var propertyValue = shoppingCard[propertyName];
+//        console.log(propertyName, propertyValue)
+// }
+// for(propertyName in shoppingCard){
+//        var propertyValue = shoppingCard[propertyName];
+//        console.log(propertyName, propertyValue)
+// }
+// var color = "yellow";
+// switch(color){
+//        case "green" :
+//               console.log("you are my green friend")
+//               break;
+//        default:
+//               console.log("You Aere my Black beauty")
+// }
